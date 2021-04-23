8f441f51989ce3e292ec879a8089ecdbc4be8552
Ринас Хисамутдинов <rinas.ka@yandex.ru>
2021-04-23 16:43:21 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# romarus.github.io
+:zap: Profile site
+
+Install and start:
+```
+yarn
+yarn start
+```
---
+++ src/containers/App/App.jsx
@@ -0,0 +1,166 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import backgroundImageUrl from "./background.jpg";
+import backgroundImageUrl90 from "./background-90.jpg";
+import backgroundImageUrl80 from "./background-80.jpg";
+
+const Root = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+  align-items: center;
+  background-image: url("${backgroundImageUrl}");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  @media screen and (max-width: 767px) {
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1366px) {
+    background-image: url("${backgroundImageUrl90}");
+  }
+
+  @media screen and (max-width: 768px) {
+    background-image: url("${backgroundImageUrl80}");
+  }
+`;
+
+const TextBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 0 128px;
+  color: white;
+  z-index: 1;
+
+  @media screen and (max-width: 767px) {
+    margin: 8px;
+  }
+`;
+
+const Name = styled.h1`
+  font-size: 64px;
+  width: 100%;
+  margin: 0;
+
+  @media screen and (max-width: 767px) {
+    font-size: 32px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 4px;
+  font-weight: 200;
+  font-size: 24px;
+  background: #47ffd1; /* Old Browsers */
+  background: -webkit-linear-gradient(left, #47ffd1, #174d32); /*Safari 5.1-6*/
+  background: -o-linear-gradient(left, #47ffd1, #174d32); /*Opera 11.1-12*/
+  background: -moz-linear-gradient(left, #47ffd1, #174d32); /*Fx 3.6-15*/
+  background: linear-gradient(to right, #47ffd1, #174d32); /*Standard*/
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  @media screen and (max-width: 767px) {
+    font-size: 18px;
+  }
+`;
+
+const DescriptionLink = styled.a``;
+
+const Copyright = styled.p`
+  position: absolute;
+  right: 32px;
+  bottom: 32px;
+  color: white;
+  margin: 0;
+
+  @media screen and (max-width: 767px) {
+    right: 8px;
+    bottom: 8px;
+  }
+`;
+
+const AlfabankLink = () => (
+  <DescriptionLink href="https://alfabank.ru" target="_blank" rel="noopener">
+    alfabank.ru
+  </DescriptionLink>
+);
+
+const Links = styled.div`
+  color: white;
+  position: absolute;
+  bottom: 32px;
+  left: 32px;
+  font-size: 28px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 767px) {
+    bottom: 8px;
+    left: 8px;
+  }
+`;
+
+const SocietyLinkTag = styled.a`
+  color: white;
+  margin-bottom: 6px;
+
+  :last-child {
+    margin-bottom: 0;
+  }
+
+  :visited {
+    color: white;
+  }
+`;
+
+const BackgroundShadow = styled.div`
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100%;
+  width: 100%;
+
+  @media screen and (max-width: 767px) {
+    background-color: rgba(0, 0, 0, 0.66);
+  }
+`;
+
+const SocietyLink = ({ href, children }) => (
+  <SocietyLinkTag href={href} target="_blank" rel="noopener">
+    {children}
+  </SocietyLinkTag>
+);
+
+export class App extends Component {
+  render() {
+    return (
+      <Root>
+        <BackgroundShadow />
+        <TextBlock>
+          Hi, I'm @romarus
+          <Name>Khisamutdinov Rinas</Name>
+          <Description>
+            Software engineer at <AlfabankLink />
+          </Description>
+        </TextBlock>
+        <Copyright>Dots & rays by Tomislav Jakupec</Copyright>
+        <Links>
+          <SocietyLink href="https://vk.com/id33255365">
+            <i className="fab fa-vk" />
+          </SocietyLink>
+          <SocietyLink href="https://www.instagram.com/rinas.kaa/">
+            <i className="fab fa-instagram" />
+          </SocietyLink>
+          <SocietyLink href="https://github.com/devromarus/">
+            <i className="fab fa-github" />
+          </SocietyLink>
+          <SocietyLink href="https://telegram.me/rinaska">
+            <i className="fab fa-telegram" />
+          </SocietyLink>
+        </Links>
+      </Root>
+    );
+  }
+}
